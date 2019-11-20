21833c9897d8fdcb7fd6c8d11a2c75a8f1a0eb0f
Alexey Ivankov <alexey.ivankov@biz-tek.ru>
2019-11-20 18:55:47 +0300

    Initial Commit

---
+++ DataTrain/DataTrain.h
@@ -0,0 +1,19 @@
+//
+//  DataTrain.h
+//  DataTrain
+//
+//  Created by Alexey Ivankov on 20.11.2019.
+//  Copyright © 2019 ivankov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataTrain.
+FOUNDATION_EXPORT double DataTrainVersionNumber;
+
+//! Project version string for DataTrain.
+FOUNDATION_EXPORT const unsigned char DataTrainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataTrain/PublicHeader.h>
+
+
